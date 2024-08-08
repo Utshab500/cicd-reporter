b5dc238709d02b3c5e680a75123e81e7691bd7ca
Utshab Saha <utshab.saha@accenture.com>
2024-08-08 15:35:55 +0800

    first commit

---
+++ test-summary.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+RESP=$(curl --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" "https://sgts.gitlab-dedicated.com/api/v4/projects/8568/pipelines")
+# echo "test"
+# echo $RESP
+python pipeline-summary.py --action="markdown" $RESP > test.md
+sed -i'.back' 's/web_url/IGNORE_WEB_URL/g' test.md
+sed -i'.back' 's/sha/IGNORE_SHA/g' test.md
+sed -i'.back' 's/ref/COMMIT_BRANCH_NAME/g' test.md
+sed -i'.back' 's/source/PIPELINE_TRIGGER/g' test.md
+RESP=$(cat test.md)
+export CONTENT=$(python pipeline-summary.py --action="summary" "$RESP")
+# echo $CONTENT > test.txt
+envsubst < $(pwd)/newsletter-template.html > newsletter.html
+
