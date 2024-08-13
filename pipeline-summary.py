cd4996d94f4f393019bb3f7389dbb2fdae6d4b7c
Utshab Saha <utshab.saha@accenture.com>
2024-08-13 16:13:04 +0800

    argument passing is changed

--- pipeline-summary.py
+++ pipeline-summary.py
@@ -13,8 +13,8 @@ from src.modules.ReportFormatter import ReportFormatter
 # --action = [ markdown, summary ]
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="A script with more options.")
-  parser.add_argument('response', type=str, help='The curl response')
-  parser.add_argument('-a', '--action', type=str, help='Specify a action')
+  parser.add_argument("--response", required=True, type=str, help='The curl response')
+  parser.add_argument("--action", required=True, type=str, help='Specify a action')
   args = parser.parse_args()
   # print(args.action)
   if args.action == "markdown":
--- test-summary.sh
+++ test-summary.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
 # RESP=$(curl --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" "https://sgts.gitlab-dedicated.com/api/v4/projects/34770/pipelines")
-# RESP=$(cat ./dummy-data/d1.json)
-export TRACK=$(python pipeline-summary.py --action="title" "$RESP")
+RESP=$(cat ./dummy-data/d1.json)
+export TRACK=$(python pipeline-summary.py --action=title --response="$RESP")
 # echo "test"
 # echo $RESP
-python pipeline-summary.py --action="markdown" "$RESP" > test.md
+python pipeline-summary.py --action=markdown --response="$RESP" > test.md
 sed -i'.back' 's/web_url/IGNORE_WEB_URL/g' test.md
 sed -i'.back' 's/sha/IGNORE_SHA/g' test.md
 sed -i'.back' 's/ref/COMMIT_BRANCH_NAME/g' test.md
 sed -i'.back' 's/source/PIPELINE_TRIGGER/g' test.md
 RESP=$(cat test.md)
-export CONTENT=$(python pipeline-summary.py --action="summary" "$RESP")
+export CONTENT=$(python pipeline-summary.py --action=summary --response="$RESP")
 # echo $CONTENT > test.txt
 envsubst < $(pwd)/newsletter-template.html > newsletter.html
 
